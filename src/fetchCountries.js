2e4bf305c9bf2c9e1cd84391a5d5c92d492f9c82
Ostap Pluzhnik <plostvik@gmail.com>
2020-10-30 00:39:19 +0200

    Закончил задачу

---
+++ src/fetchCountries.js
@@ -0,0 +1,22 @@
+import { error } from "../node_modules/@pnotify/core/dist/PNotify.js";
+// import * as PNotifyMobile from "../node_modules/@pnotify/mobile/dist/PNotifyMobile.js";
+import "../node_modules/@pnotify/core/dist/PNotify.css";
+import "../node_modules/@pnotify/mobile/dist/PNotifyMobile.css";
+import "../node_modules/@pnotify/core/dist/BrightTheme.css";
+
+export default function (searchQuery) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((response) => {
+      if (response.length > 10) {
+        error("Too many matches found. Please enter a more specific query");
+      } else return response;
+    })
+    .catch(() => {
+      error(`Error can't find country with this name`);
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import css from "./css/style.css";
+import fetchCountries from "./fetchCountries.js";
+import countryTemplate from "./countryTemplate.hbs";
+// import debounce from "../node_modules/lodash.debounce/index.js";
+var debounce = require("lodash.debounce");
+
+const search = document.querySelector(".search");
+const countryList = document.querySelector(".country-list");
+const countryDiv = document.querySelector(".country-wrapper");
+
+const render = function (event) {
+  countryList.innerHTML = "";
+  countryDiv.innerHTML = "";
+  let result;
+  if (event.target.value !== "") {
+    result = fetchCountries(event.target.value);
+    result.then((result) => {
+      if (result) {
+        if (result.length >= 2 && result.length <= 10) {
+          result.forEach((el) => {
+            countryList.innerHTML += `<li class="country-list-item">${el.name}</li>`;
+          });
+          countryList.addEventListener("click", (event) => {
+            search.value = event.target.textContent;
+            result.forEach((el) => {
+              if (el.name === event.target.textContent) {
+                countryList.innerHTML = "";
+                countryDiv.innerHTML = countryTemplate(el);
+              }
+            });
+          });
+        } else {
+          countryDiv.innerHTML = countryTemplate(result[0]);
+        }
+      }
+    });
+  }
+};
+
+search.addEventListener(
+  "input",
+  debounce((event) => render(event), 500),
+);
